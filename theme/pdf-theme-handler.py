042a87153ac7d2a47a3b6a268589347fb312e7d1
Evgeniy Devyatykh <e.devyatyh@squaregps.com>
2024-01-17 14:41:23 +0500

    try to fix build

--- theme/pdf-theme-handler.py
+++ theme/pdf-theme-handler.py
@@ -1,24 +1,57 @@
 def get_stylesheet() -> str:
     return """
-    .navixy-hide-in-pdf {
-        visibility: hidden;
+    h1, h2, h3 {
+         string-set: chapter content();
     }
-    @page {
-        size: a4 portrait;
-        margin: 25mm 10mm 25mm 10mm;
-        counter-increment: page;
-        font-family: "Roboto","Helvetica Neue",Helvetica,Arial,sans-serif;
-        white-space: pre;
-        color: grey;
-        @top-left {
-            content: "Copyright © 2020 Navixy";
-        }
-        @top-center {
-            content: string(chapter);
-        }
+    
+    .md-container {
+        display: block;
+        padding-top: 0;
+    }
+    
+    .md-main {
+        display: block;
+        height: inherit;
+    }
+    
+    .md-main__inner {
+        height: inherit;
+        padding-top: 0;
+    }
+    
+    .md-typeset .codehilitetable .linenos {
+        display: none;
+    }
+    
+    .md-typeset .footnote-ref {
+        display: inline-block;
+    }
+    
+    .md-typeset a.footnote-backref {
+        transform: translateX(0);
+        opacity: 1;
+    }
+    .md-typeset .admonition {
+        display: block;
+        border-top: .1rem solid rgba(0,0,0,.07);
+        border-right: .1rem solid rgba(0,0,0,.07);
+        border-bottom: .1rem solid rgba(0,0,0,.07);
+        page-break-inside: avoid;
+    }
+    
+    .md-typeset a::after {
+        color: inherit;
+        content: none;
+    }
+    
+    .md-typeset table:not([class]) th {
+        min-width: 0;
+    }
+    
+    .md-typeset table {
+        border: .1rem solid rgba(0,0,0,.07);
     }
     """
-
 
 # download link manually added to header
 def modify_html(html: str, href: str) -> str:
