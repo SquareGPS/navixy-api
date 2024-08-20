0e3a5123c0b9b2281d1b2ae6d3cccaa7a462e480
danielabelski <daniel.abelski@gmail.com>
2024-08-20 17:30:16 +0500

    PDF export removed

--- build.cmd
+++ build.cmd
@@ -1,2 +1,1 @@
-SET ENABLE_PDF_EXPORT=1
 CALL mkdocs build --clean --strict --use-directory-urls --site-dir ./.site
--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-export ENABLE_PDF_EXPORT=1
 mkdocs build --clean --strict --use-directory-urls --site-dir ./.site
